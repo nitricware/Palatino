270599edffdd4e211ab903e7b88de6c0fc140f75
Kurt Höblinger <kurt@key-largo.local>
2018-12-19 19:37:59 +0100

    Initial Commit

---
+++ Palatino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Palatino
+//
+//  Created by Kurt Höblinger on 19.12.18.
+//  Copyright © 2018 Kurt Höblinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PalatinoTests/PalatinoTests.swift
@@ -0,0 +1,34 @@
+//
+//  PalatinoTests.swift
+//  PalatinoTests
+//
+//  Created by Kurt Höblinger on 19.12.18.
+//  Copyright © 2018 Kurt Höblinger. All rights reserved.
+//
+
+import XCTest
+@testable import Palatino
+
+class PalatinoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
